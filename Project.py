af89a890ec62a0f3e871b992de1f1f26cda093a4
Denis <denis.bondar111@gmail.com>
2019-04-09 19:35:09 +0300

    Added Pi calculation, made full table print, added pirson calculations

--- Project.py
+++ Project.py
@@ -1,17 +1,83 @@
+import math
+from scipy.integrate import quad
+import sympy as sp
+import numpy as np
+
+a = 0
+sigma = 0
 n = 9
 k = 11
 alfa = 0.05
-
-X = [(300 + (100 * i),300 + 100 * (i+1)) for i in range(n)]
-N = [1,9 + k,18,33,40 - k, 52 - k, 29, 14 + k, 4]
-Sum = sum(N)
+X = [[100 * i,100 * (i+1)] for i in range(n)]
+M = [1,9 + k,18,33,40 - k, 52 - k, 29, 14 + k, 4]
+P = []
+Sum = sum(M)
 
 def printTable():
-    print('\nTerm of service','\t','Number of transistors')
-    for x,n in zip(X,N):
-        print(x[0],'-',x[1],"\t\t",n)
-    print("In all:",'\t\t',Sum)
+    print('\nTerm of service','\t','Number of transistors','\t\t','Pi')
+    for x,m,p in zip(X,M,P):
+        print(x[0],'-',x[1],"\t\t",m,"\t\t\t\t",p)
+    print("In all:",'\t\t',Sum,'\t\t\t\t',sum(P))
+def mergeIfNeeded():
+    global n
+    for i in reversed(range(len(M))):
+        if M[i] < 5:
+            if i < n - 1:
+                M[i+1] += M[i]
+                X[i+1][0] = X[i][0]
+                X.pop(i)
+                M.pop(i)
+                n -= 1
+            else:
+                M[i-1] += M[i]
+                X[i-1][1] = X[i][1]
+                X.pop(i)
+                M.pop(i)
+                n -= 1
+def average(keys,values):
+    Sum = 0
+    for key,val in zip(keys,values):
+        Sum += key*val
+    return Sum/sum(M)
+def deviation(keys,values,avg):
+    Sum = 0
+    for key,val in zip(keys,values):
+        Sum += val * ((key - avg) ** 2)
+    return Sum
+def f(x):
+    return math.exp((-(x-a)**2)/(2*sigma**2))
+def pirson():
+    return sum([(((m - n*p)**2)/n*p) for m,p in zip(M,P)])
+
+def method():
+    global a,sigma,P
+    a = average([((i[0] + i[1]) / 2) for i in X], M)
+    sigma = math.sqrt(1/(n-1) * deviation([((i[0] + i[1]) / 2) for i in X], M,a))
+    print('\na:',a,'\nσ:',sigma)
+    for i,x in enumerate(X):
+        if i > 0 and i < n-1:
+            P.append((1/(sigma * math.sqrt(2*np.pi))) * (quad(f,x[0],x[1])[0]))
+        if i == 0:
+            P.append((1/(sigma * math.sqrt(2*np.pi))) * (quad(f,-np.inf,x[1])[0]))
+        if i == n-1:
+            P.append((1/(sigma * math.sqrt(2*np.pi))) * (quad(f,x[0],np.inf)[0]))
+    emp = pirson()
+    print('Χ²:',emp)
+    df = n - 3
+    print('\ndf:',df)
+    print('Alpha:',alfa)
+    return emp
+        
+    
+
+
+
 
 def main():
+    mergeIfNeeded()
+    emp = method()
     printTable()
+    crit = float(input('\nInput:'))
+    if emp < crit: print('The hipotesis is true')
+    else: print('The hipotesis is false')
 main()
